3e25a4606efc6998567a9246df4e0362e334e165
MargoGraveren <garbarz.paulina@gmail.com>
2020-06-17 15:46:21 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Music Player</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div class="music-container">
+    <div class="music-header">
+        <i class="fa fa-angle-left"></i>
+        <div class="title"><p>My Music</p></div>
+        <i class="fa fa-search"></i>
+    </div>
+
+    <div class="music-playlist">
+        <div class="music-menu text-center">
+            <div class="menu">
+                <ul>
+                    <li><a href="#">Songs</a></li>
+                    <li><a href="#">Albums</a></li>
+                    <li><a href="#">Artists</a></li>
+                    <li><a href="#">Genres</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="music-box">
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/1.jpg">
+                </div>
+                <div class="music-name">
+                    <h6>Waves</h6>
+                    <p>Dean Lewis</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/3.jpg">
+                </div>
+                <div class="music-name">
+                    <h6>Before You Go</h6>
+                    <p>Lewis Capaldi</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/3.jpg">
+                </div>
+                <div class="music-name">
+                    <h6>Hold Me While You Wait</h6>
+                    <p>Lewis Capaldi</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/4.PNG">
+                </div>
+                <div class="music-name">
+                    <h6>The Bird</h6>
+                    <p>SYML</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/6.jpg">
+                </div>
+                <div class="music-name">
+                    <h6>Body</h6>
+                    <p>SYML</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/2.PNG">
+                </div>
+                <div class="music-name">
+                    <h6>Be Alright</h6>
+                    <p>Dean Lewis</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/5.jpg">
+                </div>
+                <div class="music-name">
+                    <h6>Hurt for Me</h6>
+                    <p>SYML</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/5.jpg">
+                </div>
+                <div class="music-name">
+                    <h6>Fear of the Water</h6>
+                    <p>SYML</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/1.jpg">
+                </div>
+                <div class="music-name">
+                    <h6>A Place We Knew</h6>
+                    <p>Dean Lewis</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+
+            <div class="music-info">
+                <div class="music-img">
+                    <img src="images/4.PNG">
+                </div>
+                <div class="music-name">
+                    <h6>Girl</h6>
+                    <p>SYML</p>
+                </div>
+                <i class="fa fa-ellipsis-v"></i>
+            </div><hr>
+        </div>
+
+        <div class="music-play">
+            <div class="play-image">
+                <img src="images/4.PNG">
+            </div>
+
+            <div class="play-controls">
+                <div class="controls">
+                    <div class="song-name">
+                        <h5>The Bird</h5>
+                        <p>SYML</p>
+                    </div>
+
+                    <div class="play-icon">
+                        <i class="fa fa-step-backward"></i>
+                        <i class="fa fa-play"></i>
+                        <i class="fa fa-step-forward"></i>
+                    </div>
+                </div>
+
+                <div class="music-progress">
+                    <div class="progress">
+                        <div class="progress-bar">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
